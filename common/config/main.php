947de4a3f8ff185a8f7fa244eac42315b98d19be
Igor N <degetal-virtual@yandex.ua>
2016-09-12 12:19:45 +0300

    locale settings

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,21 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'forceTranslation' => true,
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/../messages',
+                    'sourceLanguage' => 'en',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'app/form' => 'form.php',
+                        'app/text' => 'text.php',
+                        'app/menu' => 'menu.php',
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
