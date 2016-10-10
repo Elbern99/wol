23400f4eb676a6c544188d5e11e8260a7dbb2c26
malikov <dmalikov@qualium-systems.com.ua>
2016-10-10 11:38:39 +0300

    added map and base styles

--- frontend/views/article/map.php
+++ frontend/views/article/map.php
@@ -1,29 +1,25 @@
 <?php
 /* @var $this yii\web\View */
+$config = [
+  'json_path' => '/json/countries.geo.json',
+  //'json_path_countires' => json_decode(file_get_contents('http://iza.loc/json/economytypes.json'));
+  'json_path_country' => '/json/countrydata.json',
+  'json_path_economytypes' => '/json/economytypes.json'
+];
 
-$this->registerJsFile('https://www.gstatic.com/charts/loader.js');
-$this->registerJs("google.charts.load('upcoming', {'packages':['geochart']});
-      google.charts.setOnLoadCallback(drawRegionsMap);
-
-      function drawRegionsMap() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Country', 'Popularity'],
-          ['Germany', 200],
-          ['United States', 300],
-          ['Brazil', 400],
-          ['Canada', 500],
-          ['France', 600],
-          ['RU', 700]
-        ]);
-
-        var options = {};
-
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
-        chart.draw(data, options);
-      }
-");
+$this->registerJsFile('https://unpkg.com/leaflet@1.0.1/dist/leaflet.js');
+$this->registerJsFile('/js/icon.label.js');
+$this->registerJsFile('/js/map.js', ['depends'=>['yii\web\YiiAsset']]);
+$this->registerCssFile('/css/leaflet.css');
 ?>
-<div id="regions_div" style="width: 900px; height: 500px;"></div>
+<script type="text/javascript">
+  var mapConfig = <?= json_encode($config) ?>
+</script>
 
+<div class="map-holder">
+  <div class="map-info">
+    <div class="map-info-content"></div>
+    <div class="icon-close"></div>
+  </div>
+  <div id="map"></div>
+</div>
