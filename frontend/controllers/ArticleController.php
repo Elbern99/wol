656d0658f69de99104d949bb1d18f7f5891dcd6f
Igor N <degetal-virtual@yandex.ua>
2016-12-02 16:17:16 +0200

    fix category url in menu

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -55,6 +55,7 @@ class ArticleController extends Controller {
                                  return $query->select(['category_id', 'article_id']);
                              }])
                              ->orderBy(['created_at' => $order])
+                             ->limit(10)
                              ->all();
         
         $articlesIds = ArrayHelper::getColumn($articles, 'id');
