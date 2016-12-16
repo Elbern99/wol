38bfd7feecd66283178646e1903abc6a3913ee06
malikov <dmalikov@qualium-systems.com.ua>
2016-12-16 16:36:36 +0200

    fixed search

---
+++ frontend/views/html/my-account.php
@@ -0,0 +1,176 @@
+<div class="my-account-page">
+    <div class="account-head-tabs">
+        <div class="account-head-holder">
+            <div class="container">
+                <div class="breadcrumbs">
+                    <ul class="breadcrumbs-list">
+                        <li><a href="">home</a></li>
+                        <li>contact us</li>
+                    </ul>
+                </div>
+
+                <div class="account-head">
+                    <div class="img">
+                        <a href=""></a>
+                    </div>
+                    <h1>Steve Smith</h1>
+                </div>
+            </div>
+        </div>
+        <div class="account-tabs">
+            <div class="container">
+                <a href="" class="account-delete">delete account</a>
+                <ul class="account-tabs-list">
+                    <li class="active"><a href="">Your profile</a></li>
+                    <li><a href="">Favorite articles</a></li>
+                    <li><a href="">Saved searches</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-account-page">
+        <div class="account-content">
+            <div class="line">
+                <div class="label-holder">Name</div>
+                <div class="desc">
+                    Steve Smith <a href="" class="edit">edit</a>
+                </div>
+            </div>
+            <div class="line">
+                <div class="label-holder">Email address</div>
+                <div class="desc">
+                    steve@smith.com <a href="" class="edit">edit</a>
+                </div>
+            </div>
+            <div class="line">
+                <div class="label-holder">Password</div>
+                <div class="desc"><a href="" class="edit-password">change password</a></div>
+            </div>
+            <div class="line">
+                <div class="label-holder">
+                    Areas of interest
+                </div>
+                <div class="desc">
+                    <div class="grid">
+                        <div class="grid-line one">
+                            <div class="grid-item form-item">
+                                <div class="select-clear-all">
+                                    <span class="clear-all">Clear all</span>
+                                    <span class="select-all">Select all</span>
+                                    <div class="tooltip-dropdown dropdown left">
+                                        <div class="icon-question tooltip"></div>
+                                        <div class="tooltip-content drop-content">
+                                            <div class="icon-close"></div>
+                                            <strong>At vero eos et accusamus</strong>
+                                            Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis volupt atibus maiores alias consequatur aut per ferendis recusan ciedae doloribus asperiores.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid-line four">
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Program evaluation</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Behavioral and personnel economics</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Migration and ethnicity</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Labor markets and institutions</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Transition and emerging economies</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Development</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Environment</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Education and human capital</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Demography, family, and gender</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="grid-item">
+                                <div class="form-item">
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" name="">
+                                        <span class="label-text">Data and methods</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="line line-subscriptions">
+                <div class="label-holder">Subscriptions</div>
+                <div class="desc">
+                    <div class="form-line">
+                        <label class="def-checkbox">
+                            <input type="checkbox" name="">
+                            <span class="label-text">Subscribe to IZA World of Labor newsletter</span>
+                        </label>
+                    </div>
+                    <div class="form-line">
+                        <label class="def-checkbox">
+                            <input type="checkbox" name="">
+                            <span class="label-text">Subscribe to Article Alerts</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
