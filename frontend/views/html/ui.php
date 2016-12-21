c1f823d1b076bcb32d3ec2dbc78456f1d9834502
malikov <dmalikov@qualium-systems.com.ua>
2016-12-21 16:37:26 +0200

    add animate scss

---
+++ frontend/views/html/ui.php
@@ -0,0 +1,17 @@
+<div class="without-breadcrumbs">
+    <div class="container">
+        <h2>BUTTONS</h2>
+        <ol>
+            <li><a href="" class="btn-blue">Blue button</a></li>
+            <li><a href="" class="btn-blue-large">Blue button large</a></li>
+            <li><a href="" class="btn-border-blue">Blue border button</a></li>
+            <li><a href="" class="btn-border-gray-middle short"><span class="icon-heart"></span></a></li>
+            <li><a href="" class="btn-border-blue-middle with-icon" target="_blank">
+                    <div class="inner"><span class="icon-print"></span><span>print all references</span></div>
+                </a></li>
+            <li>
+                <input type="text" class="form-control">
+            </li>
+        </ol>
+    </div>
+</div>
