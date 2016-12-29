1fb9231f1aae2977545bb459747f18421cbf465f
malikov <dmalikov@qualium-systems.com.ua>
2016-12-29 15:32:08 +0200

    added opinion item

---
+++ frontend/views/html/opinion-items.php
@@ -0,0 +1,171 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+?>
+
+<?php
+$this->registerJsFile('/js/pages/opinions.js', ['depends' => ['yii\web\YiiAsset']]);
+?>
+
+<div class="container opinions-page">
+    <div class="article-head-holder">
+        <div class="article-head">
+            <div class="breadcrumbs">
+                <ul class="breadcrumbs-list"><li><a href="/">Home</a></li>
+                    <li><a href="/articles">articles</a></li>
+                </ul>
+            </div>
+            <div class="mobile-filter-holder custom-tabs-holder">
+                <ul class="mobile-filter-list">
+                    <li><a href="">Opinions</a></li>
+                    <li><a href="">Videos</a></li>
+                </ul>
+                <div class="mobile-filter-items custom-tabs">
+                    <div class="tab-item js-tab-hidden expand-more">
+                        <ul class="sidebar-news-list">
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                        </ul>
+                        <a href="" class="more-link">
+                            <span class="more">More</span>
+                            <span class="less">Less</span>
+                        </a>
+                    </div>
+                    <div class="tab-item js-tab-hidden expand-more">
+                        <ul class="sidebar-news-list">
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/where-do-immigrants-retire-to">Where do immigrants retire to?</a></h3>
+                                <div class="writer">Augustin De Coulon</div>
+                            </li>
+                            <li>
+                                <h3><a href="/articles/pros-and-cons-of-workplace-tournaments">The pros and cons of workplace tournaments</a></h3>
+                                <div class="writer">Roman M. Sheremeta</div>
+                            </li>
+                        </ul>
+                        <a href="" class="more-link">
+                            <span class="more">More</span>
+                            <span class="less">Less</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <h1>Opinions</h1>
+            <div class="more-text-mobile">
+                <p><a href="">IZA World of Labor</a> articles provide concise, evidence-based analysis of policy-relevant topics in labor economics. We recognize that the articles will prompt discussion and possibly controversy. Opinion articles will capture these ideas and debates concisely, and anchor them with real-world examples. Opinions stated here do not necessarily reflect those of the IZA.</p>
+                <a href="" class="more-evidence-map-text-mobile"><span class="more">More</span><span class="less">Less</span></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-inner">
+        <div class="content-inner-text contact-page">
+
+        </div>
+
+        <aside class="sidebar-right">
+
+        </aside>
+    </div>
+</div>
