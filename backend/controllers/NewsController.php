fa3f40dbf0b6e5bfae17d92f23a1de4fadff7476
avardanyan <avardanyan@qualium-systems.com.ua>
2017-01-04 18:45:50 +0200

    News manager

--- backend/controllers/NewsController.php
+++ backend/controllers/NewsController.php
@@ -9,13 +9,13 @@ use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use yii\data\ActiveDataProvider;
 
-use common\models\Opinion;
+use common\models\NewsItem;
 
 
 /*
  * Opinion Manager Class Controller
  */
-class OpinionController extends Controller
+class NewsController extends Controller
 {
     public function behaviors() {
         return [
@@ -39,29 +39,31 @@ class OpinionController extends Controller
     }
     
     public function actionIndex() {
-        $opinionsQuery = Opinion::find()->orderBy('id desc');
+        $opinionsQuery = NewsItem::find()->orderBy('id desc');
         return $this->render('index', ['dataProvider' => new ActiveDataProvider(['query' => $opinionsQuery, 'pagination' => ['pageSize' => 30]])]);
     }
     
     public function actionView($id = null) {
         
         if (is_null($id)) {
-            $model = new Opinion();
+            $model = new NewsItem();
         } else {
-            $model = Opinion::findOne($id);
+            $model = NewsItem::findOne($id);
             $model->created_at = $model->created_at->format('d-m-Y');
         }
         
         if (Yii::$app->request->isPost && $model->load(Yii::$app->request->post())) {
             if ($model->saveFormatted()) {
                 if (!$id) {
-                    $message = Yii::t('app/text', 'Opinion has been added successfully.');
+                    $message = Yii::t('app/text', 'News item has been added successfully.');
+                    $path = '@web/news';
                 }
                 else {
-                    $message = Yii::t('app/text', 'Opinion has been updated successfully.');
+                    $message = Yii::t('app/text', 'News item has been updated successfully.');
+                    $path = ['/news/view', 'id' => $id];
                 }
                 Yii::$app->getSession()->setFlash('success', Yii::t('app/text', $message), false);
-                return $this->redirect('@web/opinion');
+                return $this->redirect($path);
             }
             else {
                 Yii::$app->getSession()->setFlash('error', Yii::t('app/text', implode('\n', $model->getFirstErrors())), false);
@@ -74,13 +76,13 @@ class OpinionController extends Controller
     public function actionDelete($id) {
         
         try {
-            $model = Opinion::findOne($id);
+            $model = NewsItem::findOne($id);
             if (!is_object($model)) {
                 throw new NotFoundHttpException(Yii::t('app/text', 'The requested page does not exist.'));
             }
             
             $model->delete();
-            Yii::$app->getSession()->setFlash('success', Yii::t('app/text', 'Opinion has been deleted successfully.'));
+            Yii::$app->getSession()->setFlash('success', Yii::t('app/text', 'News item has been deleted successfully.'));
             
         } catch (\yii\db\Exception $e) {
             Yii::$app->getSession()->setFlash('error', Yii::t('app/text', 'An error occurred during deletion.'));
---
+++ backend/views/news/view.php
@@ -0,0 +1,67 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\Url;
+
+use dosamigos\ckeditor\CKEditor;
+use kartik\file\FileInput;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\UrlRewrite */
+/* @var $form ActiveForm */
+$this->title = Yii::t('app.menu', 'View');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app.menu', 'News'), 'url' => Url::toRoute('/news')];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="view">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row content">
+        <div class="col-sm-12 sidenav">
+            <?php $form = ActiveForm::begin(); ?>
+
+                <?= $form->field($model, 'url_key') ?>
+                <?= $form->field($model, 'title') ?>
+                <?= $form->field($model, 'editor') ?>
+                <?= $form->field($model, 'description')->widget(CKEditor::className(), [
+                    'options' => ['rows' => 8],
+                    'preset' => 'standard',
+                    'clientOptions'=>[
+                        'enterMode' => 2,
+                        'forceEnterMode'=>false,
+                        'shiftEnterMode'=>1
+                    ]
+                ]) ?>
+                
+                <?= $form->field($model, 'short_description')->widget(CKEditor::className(), [
+                    'options' => ['rows' => 8],
+                    'preset' => 'standard',
+                    'clientOptions'=>[
+                        'enterMode' => 2,
+                        'forceEnterMode'=>false,
+                        'shiftEnterMode'=>1
+                    ]
+                ]) ?>
+            
+                <?=
+                $form->field($model, 'image_link')->fileInput()->widget(FileInput::classname(), [
+                    'options' => ['multiple' => false],
+                    'pluginOptions' => [
+                        'allowedFileExtensions' => ['jpg', 'gif', 'png', 'bmp', 'jpeg', 'jepg'],
+                        'initialPreview' => '',
+                        'initialPreviewAsData' => true,
+                        'overwriteInitial' => true,
+                        'showUpload' => false,
+                        'initialCaption' => $model->image_link,
+                        'initialPreviewConfig' => [],
+                        'overwriteInitial' => true
+                    ],
+                ]);
+                ?>
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app/form', 'Submit'), ['class' => 'btn btn-primary']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
