6a55a1ad6116c6ef49683b1c6c1bfe057b004b83
avardanyan <avardanyan@qualium-systems.com.ua>
2017-01-05 11:05:24 +0200

    Hide related articles

--- frontend/views/video/view.php
+++ frontend/views/video/view.php
@@ -121,7 +121,7 @@ $this->params['breadcrumbs'][] = $model->title;
                     </a>
                 </div>
             </div>
-
+            <?php if ($model->relatedVideos ) : ?>
             <div class="widget-title medium">related videos</div>
             <ul class="post-list media-list">
                 <?php foreach ($model->relatedVideos as $relatedVideo) : ?>
@@ -140,8 +140,9 @@ $this->params['breadcrumbs'][] = $model->title;
                     <h3><?= $video->description; ?></h3>
                 </li>
                 <?php endforeach; ?>
-              
             </ul>
+            <?php endif; ?>
+            <!--
             <div class="widget-title medium">related articles</div>
             <ul class="post-list">
                 <li class="post-item">
@@ -167,123 +168,9 @@ $this->params['breadcrumbs'][] = $model->title;
                     </div>
                     <a href="" class="article-more"><span class="more">More</span><span class="less">Less</span></a>
                 </li>
-                <li class="post-item">
-                    <ul class="article-rubrics-list">
-                        <li><a href="/subject-areas/performance-of-migrants">Performance of migrants</a></li>
-                        <li><a href="/subject-areas/migration-policy">Migration policy</a></li>
-                    </ul>
-                    <h2><a href="/articles/what-are-consequences-of-regularizing-undocumented-immigrants">What are the consequences of regularizing undocumented immigrants?</a></h2>
-                    <h3>When countries regularize undocumented
-                        residents, their work, wages, and human capital investment opportunities
-                        change</h3>
-                    <div class="publish"><a href="">Sherrie A. Kossoudji</a></div>
-                    <div class="description">
-                        Millions of people enter (or remain in)
-                        countries without permission as they flee violence, war, or economic
-                        hardship. Regularization policies that offer residence and work rights have
-                        multiple and multi-layered effects on the economy and society, but they
-                        always directly affect the labor market opportunities of those who are
-                        regularized. Large numbers of undocumented people in many countries, a new
-                        political willingness to fight for human and civil rights, and dramatically
-                        increasing refugee flows mean continued pressure to enact regularization
-                        policies.
-                    </div>
-                    <a href="" class="article-more"><span class="more">More</span><span class="less">Less</span></a>
-                </li>
-                <li class="post-item">
-                    <ul class="article-rubrics-list">
-                        <li><a href="/subject-areas/performance-of-migrants">Performance of migrants</a></li>
-                        <li><a href="/subject-areas/migration-policy">Migration policy</a></li>
-                    </ul>
-                    <h2><a href="/articles/what-are-consequences-of-regularizing-undocumented-immigrants">What are the consequences of regularizing undocumented immigrants?</a></h2>
-                    <h3>When countries regularize undocumented
-                        residents, their work, wages, and human capital investment opportunities
-                        change</h3>
-                    <div class="publish"><a href="">Sherrie A. Kossoudji</a></div>
-                    <div class="description">
-                        Millions of people enter (or remain in)
-                        countries without permission as they flee violence, war, or economic
-                        hardship. Regularization policies that offer residence and work rights have
-                        multiple and multi-layered effects on the economy and society, but they
-                        always directly affect the labor market opportunities of those who are
-                        regularized. Large numbers of undocumented people in many countries, a new
-                        political willingness to fight for human and civil rights, and dramatically
-                        increasing refugee flows mean continued pressure to enact regularization
-                        policies.
-                    </div>
-                    <a href="" class="article-more"><span class="more">More</span><span class="less">Less</span></a>
-                </li>
-                <li class="post-item">
-                    <ul class="article-rubrics-list">
-                        <li><a href="/subject-areas/performance-of-migrants">Performance of migrants</a></li>
-                        <li><a href="/subject-areas/migration-policy">Migration policy</a></li>
-                    </ul>
-                    <h2><a href="/articles/what-are-consequences-of-regularizing-undocumented-immigrants">What are the consequences of regularizing undocumented immigrants?</a></h2>
-                    <h3>When countries regularize undocumented
-                        residents, their work, wages, and human capital investment opportunities
-                        change</h3>
-                    <div class="publish"><a href="">Sherrie A. Kossoudji</a></div>
-                    <div class="description">
-                        Millions of people enter (or remain in)
-                        countries without permission as they flee violence, war, or economic
-                        hardship. Regularization policies that offer residence and work rights have
-                        multiple and multi-layered effects on the economy and society, but they
-                        always directly affect the labor market opportunities of those who are
-                        regularized. Large numbers of undocumented people in many countries, a new
-                        political willingness to fight for human and civil rights, and dramatically
-                        increasing refugee flows mean continued pressure to enact regularization
-                        policies.
-                    </div>
-                    <a href="" class="article-more"><span class="more">More</span><span class="less">Less</span></a>
-                </li>
-                <li class="post-item">
-                    <ul class="article-rubrics-list">
-                        <li><a href="/subject-areas/performance-of-migrants">Performance of migrants</a></li>
-                        <li><a href="/subject-areas/migration-policy">Migration policy</a></li>
-                    </ul>
-                    <h2><a href="/articles/what-are-consequences-of-regularizing-undocumented-immigrants">What are the consequences of regularizing undocumented immigrants?</a></h2>
-                    <h3>When countries regularize undocumented
-                        residents, their work, wages, and human capital investment opportunities
-                        change</h3>
-                    <div class="publish"><a href="">Sherrie A. Kossoudji</a>, September 2016</div>
-                    <div class="description">
-                        Millions of people enter (or remain in)
-                        countries without permission as they flee violence, war, or economic
-                        hardship. Regularization policies that offer residence and work rights have
-                        multiple and multi-layered effects on the economy and society, but they
-                        always directly affect the labor market opportunities of those who are
-                        regularized. Large numbers of undocumented people in many countries, a new
-                        political willingness to fight for human and civil rights, and dramatically
-                        increasing refugee flows mean continued pressure to enact regularization
-                        policies.
-                    </div>
-                    <a href="" class="article-more"><span class="more">More</span><span class="less">Less</span></a>
-                </li>
-                <li class="post-item">
-                    <ul class="article-rubrics-list">
-                        <li><a href="/subject-areas/performance-of-migrants">Performance of migrants</a></li>
-                        <li><a href="/subject-areas/migration-policy">Migration policy</a></li>
-                    </ul>
-                    <h2><a href="/articles/what-are-consequences-of-regularizing-undocumented-immigrants">What are the consequences of regularizing undocumented immigrants?</a></h2>
-                    <h3>When countries regularize undocumented
-                        residents, their work, wages, and human capital investment opportunities
-                        change</h3>
-                    <div class="publish"><a href="">Sherrie A. Kossoudji</a></div>
-                    <div class="description">
-                        Millions of people enter (or remain in)
-                        countries without permission as they flee violence, war, or economic
-                        hardship. Regularization policies that offer residence and work rights have
-                        multiple and multi-layered effects on the economy and society, but they
-                        always directly affect the labor market opportunities of those who are
-                        regularized. Large numbers of undocumented people in many countries, a new
-                        political willingness to fight for human and civil rights, and dramatically
-                        increasing refugee flows mean continued pressure to enact regularization
-                        policies.
-                    </div>
-                    <a href="" class="article-more"><span class="more">More</span><span class="less">Less</span></a>
-                </li>
             </ul>
             <a class="btn-gray align-center show-more" href="">show more</a>
+            -->
         </div>
         <aside class="sidebar-right">
             <div class="sidebar-buttons-holder hide-mobile">
