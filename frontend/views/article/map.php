64e4e2157143ea87e1bd8927b4b2a328b147fffb
malikov <dmalikov@qualium-systems.com.ua>
2017-01-23 18:41:48 +0200

    fixed map

--- frontend/views/article/map.php
+++ frontend/views/article/map.php
@@ -123,8 +123,13 @@ $mailBody = 'Hi.\n\n I think that you would be interested in the  following arti
 
     <div class="content-inner">
         <div class="content-inner-text">
+
+            <div class="evidence-map-text">
+                <p>The colored countries below have empirical evidence for this topic. The color indicates the country's development status, based on the country classification shown in the legend, and the number on the flag indicates how many relevant academic studies address this policy question. If you click on the flag, an overlay pops up that shows the key and additional references for an article.</p>
+                <a href="" class="more-evidence-map-text-mobile"><span class="more">More</span><span class="less">Less</span></a>
+            </div>
+
             <div class="map-holder">
-                
                 <div class="map-info">
                     <div class="map-info-inner">
                         <div class="map-info-content"></div>
@@ -142,11 +147,6 @@ $mailBody = 'Hi.\n\n I think that you would be interested in the  following arti
                 <li><div class="icon-square-yellow"></div>Factor-driven economy</li>
                 <li><div class="icon-number-reference">3</div>Number of references</li>
             </ul>
-
-            <div class="evidence-map-text">
-                <p>The colored countries below have empirical evidence for this topic. The color indicates the country's development status, based on the country classification shown in the legend, and the number on the flag indicates how many relevant academic studies address this policy question. If you click on the flag, an overlay pops up that shows the key and additional references for an article.</p>
-                <a href="" class="more-evidence-map-text-mobile"><span class="more">More</span><span class="less">Less</span></a>
-            </div>
         </div>
 
         <aside class="sidebar-right">
@@ -206,7 +206,7 @@ $mailBody = 'Hi.\n\n I think that you would be interested in the  following arti
             <div class="sidebar-widget">
                 <div class="widget-title">Article</div>
                 <a href="<?= Url::to('/articles/'.$article->seo) ?>"><?= $this->title ?></a>
-                <?= $article->availability ?>
+                <div class="writer"><?= $article->availability ?></div>
             </div>
 
             <div class="sidebar-widget">
--- frontend/views/search/items/authors.php
+++ frontend/views/search/items/authors.php
@@ -1,8 +1,6 @@
 <li class="search-results-media-item">
-    <div class="img-holder">
-        <div class="img">
-            <img src="<?= $value['avatar'] ?>" alt="<?= $value['name'] ?>">
-        </div>
+    <div class="img-holder img-holder-bg">
+        <div class="img" style="background-image: url(<?= $value['avatar'] ?>)"></div>
     </div>
     <div class="link"><a href="<?= $value['url'] ?>"><?= $value['name'] ?></a></div>
     <div class="location"><?= $value['affiliation'] ?? '' ?></div>
