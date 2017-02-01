e463334ee179a664b81296a88d572720d9f90d84
malikov <dmalikov@qualium-systems.com.ua>
2017-02-01 18:09:32 +0200

    fixed author list

--- frontend/views/authors/authors_list.php
+++ frontend/views/authors/authors_list.php
@@ -22,11 +22,42 @@ $this->registerMetaTag([
 ?>
 
 <div class="container authors-page">
+    <div class="article-head">
+        <div class="breadcrumbs">
+            <?php $this->beginContent('@app/views/components/breadcrumbs.php'); ?><?php $this->endContent(); ?>
+        </div>
+    </div>
     <div class="content-inner">
         <div class="content-inner-text">
-            <div class="article-head">
-                <div class="breadcrumbs">
-                    <?php $this->beginContent('@app/views/components/breadcrumbs.php'); ?><?php $this->endContent(); ?>
+            <div class="mobile-filter-holder custom-tabs-holder">
+                <ul class="mobile-filter-list">
+                    <li><a href="" class="js-widget">Author Search</a></li>
+                    <li><a href="" class="js-widget">Authors</a></li>
+                </ul>
+                <div class="mobile-filter-items custom-tabs">
+                    <div class="tab-item js-tab-hidden">
+                        <div class="author-search-form search">
+                            <div class="search-holder">
+                                <div class="search-top">
+                                    <span class="icon-search"></span>
+                                    <?php $form = ActiveForm::begin(['action' => '/authors']); ?>
+                                    <?= $form->field($searchModel, 'search')->textInput(['class'=>"form-control-decor", 'placeholder'=>"Enter author name"])->label('') ?>
+                                    <button type="submit" class="btn-border-blue">
+                                        <span class="inner">search</span>
+                                    </button>
+                                    <?php ActiveForm::end(); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-item blue js-tab-hidden expand-more">
+                        <?php $alphas = range('A', 'Z'); ?>
+                        <ul class="abs-list">
+                            <?php foreach ($alphas as $letter): ?>
+                                <li><a class="profile-author-letter" href="<?= Url::to(['/authors', 'filter' => $letter]) ?>"><span class="text"><?= $letter ?></span></a></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
@@ -55,18 +86,23 @@ $this->registerMetaTag([
         <aside class="sidebar-right">
             <div class="sidebar-widget sidebar-widget-articles-filter hide-mobile">
                 <ul class="sidebar-accrodion-list">
-                    <li class="sidebar-accrodion-item  is-open">
+                    <li class="sidebar-accrodion-item is-open">
                         <a href="" class="title">Author Search</a>
-                        <div class="text">
-                            <?php $form = ActiveForm::begin(['action' => '/authors']); ?>
-                                <?= $form->field($searchModel, 'search')->textInput(['class'=>"form-control-decor", 'placeholder'=>"Enter author name"])->label('') ?>
-                                <button type="submit" class="btn-blue btn-center">
-                                    <span class="inner">search</span>
-                                </button>
-                            <?php ActiveForm::end(); ?>
+                        <div class="text author-search-form search">
+                            <div class="search-holder">
+                                <div class="search-top">
+                                    <span class="icon-search"></span>
+                                    <?php $form = ActiveForm::begin(['action' => '/authors']); ?>
+                                    <?= $form->field($searchModel, 'search')->textInput(['class'=>"form-control-decor", 'placeholder'=>"Enter author name"])->label('') ?>
+                                    <button type="submit" class="btn-border-blue">
+                                        <span class="inner">search</span>
+                                    </button>
+                                    <?php ActiveForm::end(); ?>
+                                </div>
+                            </div>
                         </div>
                     </li>
-                    <li class="sidebar-accrodion-item  is-open">
+                    <li class="sidebar-accrodion-item is-open">
                         <a href="" class="title">Authors</a>
                         <div class="text">
                             <?php $alphas = range('A', 'Z'); ?>
