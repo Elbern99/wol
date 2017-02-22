98f1b16117f12b3bed8e21d4a50f3dd1e75f9338
malikov <dmalikov@qualium-systems.com.ua>
2017-02-22 10:56:24 +0200

    removed link in my account text

--- frontend/views/emails/registered.php
+++ frontend/views/emails/registered.php
@@ -3,7 +3,7 @@
 <p><span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">Welcome,</span></p>
 
 <p>
-    <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">You have successfully created your IZA World of Labor account. To manage your preferences and view your saved articles, just sign into your account here (<a href="<?=  Url::to('/my-account', true) ?>">link</a>).</span>
+    <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">You have successfully created your IZA World of Labor account. To manage your preferences and view your saved articles, just sign into your account.</span>
 </p>
 
 <p>
@@ -28,6 +28,6 @@
 <p><span style="font-size:11.0pt;font-family:'Calibri',sans-serif;color:#1f497d">&nbsp;</span></p>
 <p>
     <span style="font-size:8.0pt;font-family:'Lucida Sans',sans-serif">You are receiving this email to <a href="mailto:<?= $user->email ?>" target="_blank"><?= $user->email ?></a> as you are opted in to IZA World of Labor updates.<br>
-    You can manage your IZA World of Labor contact details and preferences at <a href="<?= Url::to('/my-account', true) ?>" target="_blank">My Account</a> or <a href="<?= Url::to(['/unsubscribe', 'number' => $subscriber], true) ?>" target="_blank">unsubscribe</a> from all IZA World of Labor emails. </span>
+    You can manage your IZA World of Labor contact details and preferences at my account or <a href="<?= Url::to(['/unsubscribe', 'number' => $subscriber], true) ?>" target="_blank">unsubscribe</a> from all IZA World of Labor emails. </span>
 </p>
 <?php $this->endContent(); ?>
