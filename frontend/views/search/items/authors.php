7a9d2f1f2091ef3ea507d714eb680197214ef652
malikov <dmalikov@qualium-systems.com.ua>
2017-03-01 12:07:02 +0200

    fixed author clickable area, changed unsubscribe message

--- frontend/views/search/items/authors.php
+++ frontend/views/search/items/authors.php
@@ -1,7 +1,7 @@
 <li class="search-results-media-item">
-    <div class="img-holder img-holder-bg">
+    <a href="<?= $value['url'] ?>" class="img-holder img-holder-bg">
         <div class="img" style="background-image: url(<?= $value['avatar'] ?>)"></div>
-    </div>
+    </a>
     <div class="name"><a href="<?= $value['url'] ?>"><?= $value['name'] ?></a></div>
     <p class="location"><?= $value['affiliation'] ?? '' ?></p>
 </li>
--- frontend/views/search/items/key_topics.php
+++ frontend/views/search/items/key_topics.php
@@ -1,7 +1,7 @@
 <li class="search-results-media-item">
-    <div class="img-holder img-holder-bg">
+    <a href="<?= '/key-topics/'.$value['url_key'] ?>" class="img-holder img-holder-bg">
         <div class="img" style="background-image: url(<?= '/uploads/topics/'.$value['image_link'] ?>)"></div>
-    </div>
+    </a>
     <div class="link"><a href="<?= '/key-topics/'.$value['url_key'] ?>">TOPIC</a></div>
     <div class="name"><a href="<?= '/key-topics/'.$value['url_key'] ?>"><?= $value['title'] ?></a></div>
     <p class="location"><?= $value['short_description'] ?></p>
