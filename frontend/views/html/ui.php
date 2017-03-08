16a5b18c6e49d7ece7ee5db0813abea62f1f0360
malikov <dmalikov@qualium-systems.com.ua>
2017-03-08 12:15:49 +0200

    fixed search select all/clear all buttons

--- frontend/views/html/ui.php
+++ frontend/views/html/ui.php
@@ -13,5 +13,98 @@
                 <input type="text" class="form-control">
             </li>
         </ol>
+        <h2>Emails</h2>
+        <h3>Email - Copy required on the email where the password has been <strong>reset</strong>.</h3>
+        <p><strong>Subject: Password reset request for your IZA World of Labor account</strong></p>
+        <p><strong>From’ email should be wol@iza.org</strong></p>
+
+        <p>--------------------------start----------------------------</p>
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">We’ve recently received a request to reset your password on your IZA World of Labor account.</span>
+        </p>
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">
+                To set your new password just click <a href="" target="_blank">[here|link]</a>.
+                If clicking that link doesn’t work, copy the URL below and paste it in your internet browser.</br><a href="" target="_blank">{link}</a>
+            </span>
+        </p>
+
+        <p><span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">If you didn’t request to set your password then you can ignore this email.</span></p>
+
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">
+                Questions? Please don’t hesitate to get in touch with us at <a href="mailto:wol.iza.org" target="_blank">wol@iza.org</a>.
+            </span>
+        </p>
+
+        <p><span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">Best wishes, </span></p>
+
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif;color:#1f497d">T</span><span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">he IZA World of Labor team <span style="color:#1f497d"><br>
+            <a href="mailto:wol.iza.org" target="_blank" style="color:black">wol.iza.org</a></span> – supporting evidence-based policy making </span>
+        </p>
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif;">
+                <a href="http://twitter.com/IZAWorldofLabor" target="_blank">twitter</a><br>
+                <a href="http://www.facebook.com/pages/IZA-World-of-Labor/174866842714452" target="_blank">facebook</a><br>
+                <a href="https://www.linkedin.com/groups/6610789/profile" target="_blank">linkedin</a>
+            </span>
+        </p>
+        <p>
+            <span style="font-size:8.0pt;font-family:'Lucida Sans',sans-serif">You are receiving this email to <a href="mailto:usermail@mail.ru" target="_blank">useremail@mail.ru</a> as you are opted in to IZA World of Labor updates.<br>
+            You can manage your IZA World of Labor contact details and preferences at my account or <a href="" target="_blank">unsubscribe</a> from all IZA World of Labor emails. </span>
+        </p>
+        <!-- footer -->
+        <p>
+            <span style="font-size:8.0pt;font-family:'Lucida Sans',sans-serif">Contact <span style="color:#1f497d"> u</span>s: <a href="mailto:wol@iza.org" target="_blank">wol@iza.org</a> - IZA World of Labor, Forschungsinstitut zur Zukunft der Arbeit GmbH (IZA), Schaumburg-Lippe-Strasse 5-9, 53113 Bonn, Germany.</span>
+        </p>
+        <p>
+            <span style="font-size:8.0pt;font-family:'Lucida Sans',sans-serif">Copyright &copy; IZA <?= date('Y') ?></span>
+        </p>
+
+        <p>------------------------end------------------------------</p>
+
+
+        <h3>Email - Copy required on the email where the password has been <strong>changed</strong>.</h3>
+        <p><strong>Subject: Your IZA World of Labor account password has been changed</strong></p>
+        <p><strong>From’ email should be wol@iza.org</strong></p>
+
+        <p>--------------------------start----------------------------</p>
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">We’ve recently received a request to reset your password on your IZA World of Labor account.</span>
+        </p>
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">
+                Your IZA World of Labor password has been changed.<br>
+                If you didn’t ask to change your password, and you think that someone might have accessed your account, then get in touch with us at <a href="mailto:wol.iza.org" target="_blank">wol@iza.org</a>
+            </span>
+        </p>
+
+        <p><span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">Best wishes, </span></p>
+
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif;color:#1f497d">T</span><span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif">he IZA World of Labor team <span style="color:#1f497d"><br>
+            <a href="mailto:wol.iza.org" target="_blank" style="color:black">wol.iza.org</a></span> – supporting evidence-based policy making </span>
+        </p>
+        <p>
+            <span style="font-size:10.0pt;font-family:'Lucida Sans',sans-serif;">
+                <a href="http://twitter.com/IZAWorldofLabor" target="_blank">twitter</a><br>
+                <a href="http://www.facebook.com/pages/IZA-World-of-Labor/174866842714452" target="_blank">facebook</a><br>
+                <a href="https://www.linkedin.com/groups/6610789/profile" target="_blank">linkedin</a>
+            </span>
+        </p>
+        <p>
+            <span style="font-size:8.0pt;font-family:'Lucida Sans',sans-serif">You are receiving this email to <a href="mailto:usermail@mail.ru" target="_blank">useremail@mail.ru</a> as you are opted in to IZA World of Labor updates.<br>
+            You can manage your IZA World of Labor contact details and preferences at my account or <a href="" target="_blank">unsubscribe</a> from all IZA World of Labor emails. </span>
+        </p>
+        <!-- footer -->
+        <p>
+            <span style="font-size:8.0pt;font-family:'Lucida Sans',sans-serif">Contact <span style="color:#1f497d"> u</span>s: <a href="mailto:wol@iza.org" target="_blank">wol@iza.org</a> - IZA World of Labor, Forschungsinstitut zur Zukunft der Arbeit GmbH (IZA), Schaumburg-Lippe-Strasse 5-9, 53113 Bonn, Germany.</span>
+        </p>
+        <p>
+            <span style="font-size:8.0pt;font-family:'Lucida Sans',sans-serif">Copyright &copy; IZA <?= date('Y') ?></span>
+        </p>
+
+        <p>------------------------end------------------------------</p>
     </div>
 </div>
