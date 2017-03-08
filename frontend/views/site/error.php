5e4d717869322fb2c67518adf3a7ab0ceeabbb91
malikov <dmalikov@qualium-systems.com.ua>
2017-03-08 14:17:27 +0200

    fixed text on error page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -25,13 +25,11 @@ $this->title = $name;
                 We're sorry the page you have requested cannot be found or has moved.
             </p>
             <p>
-                <!--Please return to the <a href="/">Homepage</a>, to continue to browse IZA World of Labor.-->
                 Please return to the <a href="/">Homepage</a>,
                 use the navigation above or <a href="<?= Url::to('/search/advanced',true); ?>">search</a> to find what you are looking for.
             </p>
             <p>
-                <!--Email us to <ins>report a broken link</ins> <a href="mailto:wol@iza.org" target="_blank">wol@iza.org</a> and we'll fix it.-->
-                Email us to report a broken link <a href="mailto:wol@iza.org">wol@iza.org</a> and we'll fix it.
+                Email us to report a <a href="mailto:wol@iza.org" target="_blank">broken link</a> and we'll fix it.
             </p>
         </div>
     </div>
