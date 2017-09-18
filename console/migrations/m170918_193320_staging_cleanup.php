0ac49c3f1898606264c7c05cc303bc520bee0b6c
andriy.volberg <andriy.volberg@gmail.com>
2017-09-19 00:52:43 +0300

    more cleanup needed

--- console/migrations/m170918_193320_staging_cleanup.php
+++ console/migrations/m170918_193320_staging_cleanup.php
@@ -2,7 +2,7 @@
 
 use yii\db\Migration;
 use common\models\Category;
-
+use common\models\UrlRewrite;
 
 class m170918_193320_staging_cleanup extends Migration
 {
@@ -15,8 +15,7 @@ class m170918_193320_staging_cleanup extends Migration
 
 
     public function safeUp()
-    {/*
-     * COMMENT IT TO PREVENT USING - IT WAS RUN-ONCE fix
+    {
         for ($i = 0; $i < count($this->aliases); $i++) {
             $urlkey = $this->aliases[$i];
             $cmd = Yii::$app->db->createCommand('SELECT * FROM category WHERE url_key="' . $urlkey . '"');
@@ -29,10 +28,13 @@ class m170918_193320_staging_cleanup extends Migration
                 $right = $result['rgt'];
                 Yii::$app->db->createCommand('DELETE FROM category WHERE lft >= ' . $left . ' AND rgt <= ' . $right)->execute();
                 Yii::$app->db->createCommand('UPDATE category SET lft = IF(lft > ' . $left . ', lft - (' . $right . ' - ' . $left . ' + 1), lft), rgt = rgt - (' . $right . ' - ' . $left . ' + 1) WHERE rgt > ' . $right)->execute();
+                $rewrite = UrlRewrite::findOne(['current_path' => $urlkey]);
+                
+                if ($rewrite) {
+                    $rewrite->delete();
+                }
             }
         }
-     * 
-     */
     }
 
 
