c0cca3779e4648ce4ce4fcce17729362bd9a231e
andriy.volberg <andriy.volberg@gmail.com>
2017-09-26 00:51:10 +0300

    no line brakes

--- frontend/views/authors/profile-editor.php
+++ frontend/views/authors/profile-editor.php
@@ -72,9 +72,7 @@ $this->registerJsFile('/js/pages/profile.js', ['depends' => ['yii\web\YiiAsset']
                          ?></strong>
                         <?php $arealinks = []; ?>
                         <?php foreach($areas as $area): ?>
-                            <?php $this->beginBlock('arealink'); ?>
-                                <span><?= Html::a($area['title'], $area['url_key']) ?></span>
-                            <?php $this->endBlock(); ?>
+                            <?php $this->beginBlock('arealink'); ?><span><?= Html::a($area['title'], $area['url_key']) ?></span><?php $this->endBlock(); ?>
                             <?php $arealinks[] = $this->blocks['arealink']; ?>
                         <?php endforeach; ?>
                         <?= implode(', ', $arealinks); ?>
