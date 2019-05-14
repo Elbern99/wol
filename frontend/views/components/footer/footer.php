a19921367684e7083b098eaa8628a7e77efde507
Vladislav Dneprov <vladislav.dneprov@splynx.com>
2019-05-14 11:01:20 +0300

    IZ-5 remove google plus icon on footer

--- frontend/views/components/footer/footer.php
+++ frontend/views/components/footer/footer.php
@@ -22,7 +22,6 @@
             <li><a href="http://twitter.com/IZAWorldofLabor" target="_blank"><span class="icon-twitter"></span></a></li>
             <li><a href="http://www.linkedin.com/groups?gid=6610789&mostPopular=&trk=tyah&trkInfo=tas%3AIZA%20wo%2Cidx%3A1-1-1" target="_blank"><span class="icon-linkedn"></span></a></li>
             <li><a href="http://www.facebook.com/pages/IZA-World-of-Labor/174866842714452" target="_blank"><span class="icon-facebook"></span></a></li>
-            <li><a href="https://plus.google.com/116017394173863766515" target="_blank"><span class="icon-google"></span></a></li>
         </ul>
     </div>
 </div>
