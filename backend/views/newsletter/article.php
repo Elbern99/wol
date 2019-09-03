1d063891063781f36acad74561485564a7622705
Vladislav Dneprov <vladislav.dneprov@splynx.com>
2019-09-03 21:59:03 +0300

    IZ-12 create some styles, little bit fix

--- backend/views/newsletter/article.php
+++ backend/views/newsletter/article.php
@@ -9,6 +9,7 @@ use common\models\NewsletterLogs;
 use yii\helpers\Url;
 use yii\helpers\ArrayHelper;
 use common\models\Newsletter;
+use yii\helpers\StringHelper;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -25,11 +26,13 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-md-12">
             <?= GridView::widget([
                 'dataProvider' => $dataProvider,
+                'options' => ['id' => 'article_newsletter_logs_table'],
                 'columns' => [
-                    ['class' => 'yii\grid\SerialColumn'],
+                    ['class' => 'yii\grid\SerialColumn', 'contentOptions' => ['style'=>'vertical-align: middle;']],
                     [
                         'attribute' => 'article.title',
                         'format' => 'raw',
+                        'contentOptions' => ['style'=>'vertical-align: middle;'],
                         'value' => function (NewsletterLogs $log) {
                             return Html::a(
                                 $log->article->title,
@@ -37,10 +40,17 @@ $this->params['breadcrumbs'][] = $this->title;
                             );
                         }
                     ],
-                    'subject',
+                    [
+                        'attribute' => 'subject',
+                        'contentOptions' => ['style'=>'vertical-align: middle;'],
+                        'value' => function (NewsletterLogs $log) {
+                            return $log->subject;
+                        }
+                    ],
                     [
                         'attribute' => 'progress',
                         'format' => 'raw',
+                        'contentOptions' => ['style'=>'vertical-align: middle;'],
                         'value' => function (NewsletterLogs $log) {
                             switch ($log->status) {
                                 case NewsletterLogs::STATUS_IN_PROGRESS:
@@ -71,10 +81,18 @@ $this->params['breadcrumbs'][] = $this->title;
                             ]);
                         }
                     ],
-                    'qty',
+                    [
+                        'attribute' => 'qty',
+                        'contentOptions' => ['style'=>'vertical-align: middle; text-align: center;'],
+                        'value' => function (NewsletterLogs $log) {
+                            return $log->qty;
+                        }
+                    ],
                     [
                         'attribute' => 'subscribers',
+                        'label' => Yii::t('app', 'Subscribers emails (First 5)'),
                         'format' => 'raw',
+                        'contentOptions' => ['style'=>'vertical-align: middle;'],
                         'value' => function (NewsletterLogs $log) {
                             if (!$log->subscribers) {
                                 return Html::tag(
@@ -83,14 +101,33 @@ $this->params['breadcrumbs'][] = $this->title;
                                     ['class' => 'text-danger']
                                 );
                             }
-                            return Html::ul(ArrayHelper::getColumn($log->subscribers, function (Newsletter $subscriber) {
+                            return Html::ul(ArrayHelper::getColumn(array_slice($log->subscribers, 0, 5), function (Newsletter $subscriber) {
                                 return Html::a($subscriber->email, 'mailto:' . $subscriber->email);
-                            }), ['encode' => false]);
+                            }), ['encode' => false, 'style' => 'padding: 0; list-style:none;']);
+                        }
+                    ],
+                    [
+                        'attribute' => 'error_text',
+                        'format' => 'raw',
+                        'contentOptions' => ['style'=>'vertical-align: middle; text-align: center;'],
+                        'value' => function (NewsletterLogs $log) {
+                            if ($error = StringHelper::byteSubstr($log->error_text, 0, 150)) {
+                                return Html::tag(
+                                    'span',
+                                    strlen($log->error_text) > 170 ? $error . '...' : $error,
+                                    ['class' => 'text-danger']);
+                            }
+                            return Html::tag(
+                                'div',
+                                '',
+                                ['class' => 'text-success glyphicon glyphicon-ok']
+                            );
                         }
                     ],
                     [
                         'attribute' => 'status',
                         'format' => 'raw',
+                        'contentOptions' => ['style'=>'vertical-align: middle; text-align: center;'],
                         'value' => function (NewsletterLogs $log) {
                             switch ($log->status) {
                                 case NewsletterLogs::STATUS_IN_PROGRESS:
@@ -114,6 +151,11 @@ $this->params['breadcrumbs'][] = $this->title;
                                 ['class' => 'text-' . $textColor]
                             );
                         }
+                    ],
+                    [
+                        'attribute' => 'created_at',
+                        'format' => 'date',
+                        'contentOptions' => ['style'=>'vertical-align: middle; text-align: center;']
                     ],
                 ]
             ]); ?>
