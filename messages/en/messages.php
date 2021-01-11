49b1c287fb6b488d5353640200eeaf5b6b986c85
Dmitriy Khristenko <dkhristenko@qualium-systems.com.ua>
2021-01-11 12:07:35 +0000

    messages.php edited online with Bitbucket

--- messages/en/messages.php
+++ messages/en/messages.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'confirm_login' => "Please confirm your email address. We have resent your confirmation email to your registered address. Please check your spam folder in case the email is filed there.",
+    'confirm_login' => "Please confirm your email address. We have re-sent your confirmation email to your registered address. Please check your spam folder in case the email is filed there.",
     'wrong_login' => 'Your email or password has not been recognized. Please try again.',
     'change_email' => "We have sent a confirmation email to your new address.<br>Please confirm the change and we will update your account details.",
     'subscribe_account' => 'Your account settings have been successfully updated.'
